a63cb235c60c839f7e473ef611a200449f3761c7
Morgan Brunelle <morgan.brunelle@gmail.com>
2013-02-18 20:27:23 -0500

    Ninjas be killin em

---
+++ 5_extra_credit.py
@@ -0,0 +1,18 @@
+ninjas = 5
+if ninjas < 50 and ninjas >= 30:
+	print("That's too many")
+elif ninjas < 30 and ninjas >= 10:
+	print("It'll be a struggle, but I can take 'em")
+elif ninjas < 10:
+	print("I can fight those ninjas!")
+
+ninjas = 5
+if ninjas < 10:
+	print("I can fight those ninjas!")
+elif ninjas >= 10 and ninjas < 30:
+	print("It'll be a struggle, but I can take 'em")
+elif ninjas >= 30 and ninjas < 50:
+	print("That's too many")
+else:
+	print("That's waaaaay too many!")
+
