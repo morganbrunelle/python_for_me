3fb3596aec7c113702da6232992cf55720c56761
Morgan Brunelle <morgan.brunelle@gmail.com>
2013-03-10 19:02:09 -0400

    Good good sonnings.

---
+++ cans.py
@@ -0,0 +1,8 @@
+def spaceship_building(cans):
+	total_cans = 0
+	for week in range(1, 53):
+		total_cans = total_cans + cans
+		print('Week %s = %s cans' % (week, total_cans))
+
+
+spaceship_building(3)
---
+++ silly_age_joke.py
@@ -0,0 +1,7 @@
+def silly_age_joke(age):
+	if age >= 10 and age <= 13:
+		print("What is 13 + 49 + 84 + 155 + 97? A headache!")
+	else:
+		print("Huh?")
+
+silly_age_joke(11)
