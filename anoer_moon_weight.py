cd78ad1363012d4e18531fdd8b76e85e9e2cb48d
Morgan Brunelle <morgan.brunelle@gmail.com>
2013-03-12 19:28:59 -0400

    Met fuckings

---
+++ anoer_moon_weight.py
@@ -0,0 +1,9 @@
+#write a program that asks the user what number do you want to square and print that square
+
+def square(number):
+    print(number * number)
+
+print("What number do you want to square?")
+
+number = int(input())
+square(number)
