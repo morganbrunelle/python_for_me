a08c8e1ced671fb8bc0ceb9bb5f47173bffe56d5
Morgan Brunelle <morgan.brunelle@gmail.com>
2013-06-01 19:31:53 -0700

    Tayo helped me.

--- shuffle.py
+++ shuffle.py
@@ -17,27 +17,14 @@ class Giraffes(Mammals):
     def eat_leaves_from_trees(self):
         print("eating leaves")
 
-    def left_foot_forward(self):
-        print('left foot forward')
-
-    def left_foot_back(self):
-        print('left foot back')
-
-    def right_foot_forward(self):
-        print('right foot forward')
-
-    def right_foot_back(self):
-        print('right foot back')
-
-    def left_foot_back(self):
-        print('left foot back')
+    def move_foot(self, foot_side, direction):
+        print(foot_side + " foot " + direction)
 
     def dance(self):
-        self.left_foot_forward()
-        self.left_foot_back()
-        self.right_foot_forward()
-        self.right_foot_back()
-        self.left_foot_back()
+        self.move_foot("left", "forward")
+        self.move_foot("left", "back")
+        self.move_foot("right", "forward")
+        self.move_foot("right", "back")
 
 reginald = Giraffes(800)
 reginald.dance()
